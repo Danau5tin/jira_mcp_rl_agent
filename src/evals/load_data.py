d83e29263eab93468b96ed95bcf1560c66056c70
Dan Austin <dan@aituning.ai>
2025-05-15 20:40:24 +0100

    Add NewEvalDataPoint class and load_example_dp function

--- src/evals/load_data.py
+++ src/evals/load_data.py
@@ -3,6 +3,9 @@ from dataclasses import dataclass
 import os
 from typing import List
 
+from src.data.dataset_entities.state_validation import ApiCallValidation, StateValidationConfig
+from src.data.dataset_entities.task_context import TaskContext
+
 @dataclass
 class EvalDataPoint:
     prompt: str
@@ -24,3 +27,44 @@ async def load_eval_data(csv_file: str) -> List[EvalDataPoint]:
             )
             eval_data_list.append(eval_data)
     return eval_data_list
+
+
+
+@dataclass
+class NewEvalDataPoint:
+    task_context: TaskContext
+    state_validation_config: StateValidationConfig
+
+    @property
+    def prompt(self) -> str:
+        """Returns the prompt for the evaluation."""
+        return self.task_context.intial_message
+
+def load_example_dp() -> List[NewEvalDataPoint]:
+    """Loads eval data from a CSV file."""
+    # TODO: Temporary until tested
+    dp = NewEvalDataPoint(
+        task_context=TaskContext(
+            goal="Create a single task in Jira",
+            intial_message="Can you create a task for me in project MBA? Title is Discover prompt automation",
+        ),
+        state_validation_config=StateValidationConfig(
+            state_validation_calls=[
+                ApiCallValidation(
+                    tool_name="jira_search",
+                    arguments={
+                        "jql": "project = MBA AND summary ~ 'Discover prompt automation'",
+                        "limit": 1,
+                    },
+                    expected_fields={
+                        "issues.0.summary": "Discover prompt automation",
+                        "issues.0.status.name": "To Do"
+                    },
+                    expected_field_presence=["issues.0.key"],
+                    expected_field_absence=None
+                )
+            ],
+        ),
+    )
+
+    return [dp]
